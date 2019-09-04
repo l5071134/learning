c4bfb0a59a726874fe94c5a84bf78fb4d8fea4d2
Macintosh <i3oy507@126.com>
2019-09-04 22:44:55 +0800

    timeit test

---
+++ python/deepLearning/basePython/timeitLIst.py
@@ -0,0 +1,60 @@
+from timeit import Timer
+
+
+# li1 = [ 1 , 2]
+
+# li2 = [ 3 , 4]
+
+# li3 = li1 + li2
+
+# li = [i for i in range(10000)]
+
+# li = list(range(10000))
+
+
+def test1():
+    li = []
+    for i in range(10000):
+        li.append(i)
+
+
+def t2():
+    li = []
+    for i in range(10000):
+        li += [i]
+
+
+def t3():
+    li = [i for i in range(10000)]
+
+
+def t4():
+    li = list(range(10000))
+
+def t5():
+    li = []
+    for i in range(10000):
+        li.extend([i])
+
+def t6():
+    li = []
+    for i in range(10000):
+        li.insert(0, i)
+
+timer1 = Timer("test1()", "from __main__ import test1")
+print("append(): ", timer1.timeit(1000))
+
+timer2 = Timer("t2()", "from __main__ import t2")
+print("+: ", timer2.timeit(1000))
+
+timer3 = Timer("t3()", "from __main__ import t3")
+print("[i for i range]: ", timer3.timeit(1000))
+
+timer4 = Timer("t4()", "from __main__ import t4")
+print("list(): ", timer4.timeit(1000))
+
+timer5 = Timer("t5()", "from __main__ import t5")
+print("extend(): ", timer5.timeit(1000))
+
+timer6 = Timer("t6()", "from __main__ import t6")
+print("extend(): ", timer6.timeit(1000))
